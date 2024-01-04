81fa97e40cfea4f988b7aa756480789ffe27a8c5
Gavin <gaving1226@gmail.com>
2024-01-04 09:16:31 -0500

    fixing main skill json

--- src/backend/src/data.py
+++ src/backend/src/data.py
@@ -1,10 +1,19 @@
 import json
 
-def loadSkills():
-    with open("../skilldata.json") as skillData:
+def initMainSkills():
+    with open("../data/mainSkills.json") as skillData:
         skills = json.load(skillData)
     
     return skills
+
+def getMainSkill(skills, name):
+    for key, value in skills.items():
+        for i in value:
+            if i["name"] == name:
+                return i
+    
+    return False
+
 
 def loadUser(userName):
     pass
