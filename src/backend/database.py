796b330d4efbcdbbba86fd2dd6e5bcd1833f9d57
Gavin <gaving1226@gmail.com>
2024-02-24 16:32:03 -0500

    restructuring backend

--- src/backend/database.py
+++ src/backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("sqlite:///../data/p6db.db",  
+engine = create_engine("sqlite:///./data/p6db.db",  
                        connect_args={"check_same_thread": False},
                        echo=True)
 
